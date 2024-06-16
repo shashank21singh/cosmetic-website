a56315bc619ab6a92aed1bbf57f1a1e20ba6ab03
shashank21singh <shashank212020@gmail.com>
2024-06-16 10:10:56 +0530

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,54 @@
+const btn = document.querySelector("button");
+const frm = document.querySelector("form");
+const fai = document.getElementById("fair");
+const pon = document.getElementById("ponds");
+const lak = document.getElementById("lakme");
+const may = document.getElementById("maybelline");
+const vlcsun = document.getElementById("vlccsun");
+const ponsun = document.getElementById("pondssun");
+const laksun = document.getElementById("lakmesun");
+const lotsun = document.getElementById("mlotussun");
+const natsun = document.getElementById("naturesun");
+
+
+
+ btn.onclick = () => {
+    alert("Button was clicked.Now you have been redirected to log in page");
+    open("http://127.0.0.1:5501/login.html");
+    }
+fai.onclick =() => {
+alert("you are taking to an official website");
+open("https://www.hul.co.in/brands/beauty-wellbeing/glow-lovely/");
+}
+pon.onclick =() => {
+    alert("you are taking to an official website");
+open("https://ponds.in/collections/best-seller?utm_source=google&utm_term=ponds&utm_creative=APSResponsive_Ads&utm_campaign=PUB_Ponds_Google_DTC_Search_BOF_Only_Brand_Sales_NA_Overall_18thOct23utm_device=Desktop_Mobile&utm_content=sem&utm_adgroup=Brand_Exact&utm_placement=&utm_medium=cpc&gad_source=1&gclid=CjwKCAjw1K-zBhBIEiwAWeCOFyKj6AhBn6uspFFDM-z6RK448r7dDCP8HA6T9Nflnur-lGINAH7OwRoC-RMQAvD_BwE");
+}
+lak.onclick =() => {
+    alert("you are taking to an official website");
+open("https://www.lakmeindia.com/?utm_source=google&utm_medium=cpc&utm_campaign=PUB_Lakme-Colours_Google_DTC_Search_Brand_NA_Sales_NA_Overall_23rdJan2024&utm_content=Brand_Exact&utm_term=lakme&gad_source=1&gclid=CjwKCAjw1K-zBhBIEiwAWeCOF03crhHS2cqSWfbvlLCOYomkqk0bSdgcXrX1ZbLBad6WCSbJYZM6CBoC03YQAvD_BwE");
+}
+may.onclick =() => {
+    alert("you are taking to an official website");
+open("https://maybelline.co.in/?utm_source=google&utm_medium=cpc&utm_campaign=&utm_content=&utm_term=maybelline&gad_source=1&gclid=CjwKCAjw1K-zBhBIEiwAWeCOFx6uWeHMunVH4iVrnSscpOy8kp6q-5n8gxJhMrah71O1Ekz2Xi13jRoCyIQQAvD_BwE");    
+}
+vlcsun.onclick = () => {
+    alert("you are taking to an official website");
+    open("https://www.vlccproducts.com/de-tan-sunscreen-gel-cream/p");
+    }
+ponsun.onclick =() => {
+alert("you are taking to an official website");
+open("https://ponds.in/products/ponds-sun-miracle-spf-50-pa-creme-gel-sunscreen-protect-hydrate-with-hyaluronic-acid?variant=46725672829161");
+}
+laksun.onclick =() => {
+    alert("you are taking to an official website");
+open("https://www.lakmeindia.com/collections/sun-protection-catalog");
+}
+lotsun.onclick =() => {
+    alert("you are taking to an official website");
+open("https://www.lotusherbals.com/collections/buy-1-get-1?gad_source=1&gclid=CjwKCAjw1K-zBhBIEiwAWeCOF1XwngPuyeY3ryAjqlX_RDK5G8Wud1geJvae9rw9RJP3N84WRzxTgxoCbx8QAvD_BwE");
+}
+natsun.onclick =() => {
+    alert("you are taking to an official website");
+open("https://www.flipkart.com/lotus-herbals-sunscreen-spf-40-pa-safesun-3in1-matte-look-daily-tinted-sunscreen-spf-pa-bb-glow/p/itm87e18a1033121?pid=SNRD92RVXZA3NPYU&lid=LSTSNRD92RVXZA3NPYUM0BJ1V&marketplace=FLIPKART&store=g9b%2Fema%2F5la%2Fxrh&srno=b_1_2&otracker=browse&fm=organic&iid=en_oMvyWGHgfN9bEXaITgneAWU9EfJ4iPraqmib_aHhKzd9YgxWcWHicZsJ2k-BL8eHiCbksbJ3Ef7-v46IbJSCEYQEIsITtCzc4bHaOMTqL08%3D&ppt=browse&ppn=browse&ssid=jy97jengwg0000001718368906005");    
+}
